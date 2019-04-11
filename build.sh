e9dd5fea1c2c948ad49503d4c4f3c45b1c19b2fb
flycorn <ym1992it@163.com>
2019-04-11 11:20:55 +0800

    update

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ mkdir -p "$out/config"
 
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./$out/todo_app  -tags v1 -v main.go
 
-sources=`find ./conf -name "*.json"`
+sources=`find ./config -name "*.json"`
 check_code
 for source in $sources;do
 	yes | echo $source|sed "s/.*\/\(.*\.json\).*/cp -f & .\/$out\/config\/\1/"|bash
